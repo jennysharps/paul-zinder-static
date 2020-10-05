2eb6bf5c3caca96a46c34cd5a02aba780448bd9b
Jenny Sharps <jenny.sharps@red-badger.com>
2020-10-05 09:09:34 +0100

    Update viewport & add validation to send email cloud fn

--- send-email/index.js
+++ send-email/index.js
@@ -1,6 +1,7 @@
+const mask = require("@moneytree/mask-pii");
 const nodemailer = require("nodemailer");
+const request = require('request');
 const log = require("./log");
-
 
 let transport = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -18,9 +19,13 @@ let transport = nodemailer.createTransport({
  * @param {!express:Response} res HTTP response context.
  */
 exports.sendEmail = async (req, res) => {
-    const allowed_origins = ["http://localhost:4000", "http://paulzinder.com", "https://jennysharps.github.io"];
+    const allowed_origins = [
+        "http://localhost:4000",
+        "http://paulzinder.com",
+        "https://jennysharps.github.io",
+    ];
     const origin = req.get('origin');
-    log.info({ origin });
+    log.info(`Origin: ${origin}`);
 
     if (allowed_origins.includes(origin)) {
         res.set('Access-Control-Allow-Origin', origin);
@@ -34,25 +39,49 @@ exports.sendEmail = async (req, res) => {
         res.status(204).send('');
         return;
     }
-        
-    // validate reCaptcha
-    // get details off of request & send email
-    log.info("Sending email...");
 
-    const message = {
-        from: 'jennylynnsharps@gmail.com',
-        to: 'jsharps85@gmail.com',
-        replyTo: req.body.email,
-        subject: 'Test email cloudfn',
-        text: `Name: ${req.body.name}\nEmail: ${req.body.email},\nMessage:\n${req.body.message}`
-    };
+    ["g-recaptcha-response", "email", "name", "message"].forEach(fieldName => {
+        if (!req.body[fieldName]) {
+            res.status(422).send({
+                status: "ERROR",
+                error: {
+                    code: "UNPROCESSABLE_ENTITY",
+                    message: `\`${fieldName}\` is a required field`
+                }
+            });
+        }
+    });
+
+    const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + process.env.RECAPTCHA_SECRET + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+    request(verificationURL, (_error, _response, body) => {
+        body = JSON.parse(body);
+
+        if(!body.success) {
+            res.status(422).send({
+                status: "ERROR",
+                error: {
+                    code: "UNPROCESSABLE_ENTITY",
+                    message: "`g-recaptcha-response` could not be validated"
+                }
+            });
+        }
+        log.info("Captcha passed validation");
+    });
 
     try {
-        const info = await transport.sendMail(message);
-        log.info({ info });
+        const message = {
+            from: 'jennylynnsharps@gmail.com',
+            to: 'jsharps85@gmail.com',
+            replyTo: req.body.email,
+            subject: 'Test email cloudfn',
+            text: `Name: ${req.body.name}\nEmail: ${req.body.email},\nMessage:\n${req.body.message}`
+        };
+
+        await transport.sendMail(message);
+        log.info(`Email sent: replyTo=${mask.email(message.replyTo)}`);
         res.status(204).send('');
     } catch (err) {
-        log.info({ err });
-        res.status(400).send({ status: "ERROR", error: err });    
+        log.info("Error sending message:", { err });
+        res.status(500).send({ status: "ERROR", error: { code: "INTERNAL_ERROR", message: "Email could not be sent" }});    
     }
 };
